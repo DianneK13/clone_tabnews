b94ed280e90c2c10c60610cb227628ba30f21d68
Maria Clara Rodrigues de Almeida <140253955+DianneK13@users.noreply.github.com>
2025-04-26 21:56:27 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Mãe, Pai... Eu amo vocês! Se algum de vocês ler essa mensagem, dê uma risadinha!</h1>
+  return (
+    <h1>
+      Mãe, Pai... Eu amo vocês! Se algum de vocês ler essa mensagem, dê uma
+      risadinha!
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
