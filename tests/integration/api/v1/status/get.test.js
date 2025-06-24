bdd36f73fc6e25e4a7f15c9b97cecf7716fce399
Maria Clara <mclarar.almeida@gmail.com>
2025-06-24 10:43:08 -0300

    removes some unnecessary comments

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -11,23 +11,4 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 
-  //const pgVersion = responseBody.dependencies.database.postgres_version;
-  //expect(pgVersion).toBeDefined();
-  //expect(pgVersion).not.toBeNull();
-  //expect(typeof pgVersion).toBe('string');
-  //expect(pgVersion).toEqual('16.0');
-
-  //const maxConnections = responseBody.max_connections;
-  //expect(maxConnections).toBeDefined;
-  //expect(maxConnections).not.toBeNull;
-  //expect(typeof maxConnections).toBe('number');
-  //console.log(maxConnections);
-
-  //const usedConnections = responseBody.used_connections;
-  //expect(usedConnections).toBeDefined;
-  //expect(usedConnections).not.toBeNull;
-  //expect(usedConnections).toBeLessThanOrEqual(maxConnections);
-  //expect(typeof usedConnections).toBe('number');
-  //console.log(usedConnections);
 });
-
